d93d181d545c22e7eaae7c0ac72319240735861d
Prakhar1808 <Prakhar1808@users.noreply.github.com>
2026-09-11 22:28:01 +0530

    feat: implement persistent storage using csv file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-expenses = []
-#TODO: store the expenses in a csv file
+import csv
+from pathlib import Path
+
+file_path = Path("expenses.csv")
 
 while True: # The loop which takes input and decides the choices
     choice = input("Enter Your Choice (add, list or quit): ").lower()
@@ -7,16 +9,25 @@ while True: # The loop which takes input and decides the choices
     if choice == "add":
         amount = input("How much is the Amount? ")
         category = input("What category of expense? ").lower()
-        expenses.append([category, amount])
+
+        with open(file_path, "a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([category, amount])
 
     elif choice == "list":
-        user_input = input("which Category to display or \"all\": ")
-        if user_input == "all":
-            print(expenses)
+        user_input = input("which Category to display or \"all\": ").lower()
+
+        if file_path.is_file():
+            print("\n['Category', 'Amount']\n----------------------------")
+
+            with open(file_path, "r") as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    if row[0] == user_input or user_input == "all":
+                        print(row)
+
         else:
-            for i in range(len(expenses)):
-                if user_input == expenses[i][0]:
-                    print(expenses[i][1])
+            print("No File Exists to read!")
 
     elif choice == "quit":
         break
