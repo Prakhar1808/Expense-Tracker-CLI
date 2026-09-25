a0d3868d22ed845dde234b32cc7283b27f15d533
Prakhar1808 <Prakhar1808@users.noreply.github.com>
2026-09-25 20:15:20 +0530

    refactor(CLI): rewrite descriptions (help) and remove terminating print statement

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from storage import add_expenses, show_expenses
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("command", choices = ["add", "show"], help = "what to do")
-parser.add_argument('-c', '--category', help = "Category of expenditre")
+parser.add_argument("command", choices = ["add", "show"], help = "choices to choose from")
+parser.add_argument('-c', '--category', help = "expenditure category")
 parser.add_argument('-a', '--amount', type = float, help = "Amount spent")
 parser.add_argument('-li', '--listcat', default = "all", help = "category specific expenditure to list")
 args = parser.parse_args()
@@ -13,5 +13,3 @@ if args.command == "add":
 
 else:
     show_expenses(args.listcat)
-
-print("Exiting Safely")
